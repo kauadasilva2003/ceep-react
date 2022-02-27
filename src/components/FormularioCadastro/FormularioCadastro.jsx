8a4367c28ddd1e32dab2fb67c3ed2aa9c56f2c66
Kauã Silva <kauad4235@gmail.com>
2022-02-26 22:09:53 -0300

    aprendendo a usar eventos no react.

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
-import "./estilo.css"
+import "./estilo.css";
 
-export default class FormularioCadastro extends Component{
-  render(){
-    return(
+export default class FormularioCadastro extends Component {
+  constructor(){
+    super();
+    this.titulo = "";
+  }
+  handleMudancaDeTitulo(evento) {
+    this.titulo = evento.target.value
+  }
+  render() {
+    return (
       <form className="form-cadastro">
-      <input type="text" className="form-cadastro_input" placeholder="Write the title"/>
-      <textarea rows={15} className="form_cadastro_input" placeholder="Write your note..."/>
-      <button className="form-cadastro_input form-cadastro_submit">Create Note</button>
-    </form>
+        <input
+          type="text"
+          className="form-cadastro_input"
+          placeholder="Write the title"
+          onChange={this.handleMudancaDeTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          className="form_cadastro_input"
+          placeholder="Write your note..."
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Create Note
+        </button>
+      </form>
     );
   }
 }
